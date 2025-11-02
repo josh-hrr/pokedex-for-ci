777b2e6af57eb80210238c618d5b307ea90e4812
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-11-02 14:26:59 -0600

    updating e2e tests eslit issues

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,22 +1,24 @@
+/* eslint-env mocha, cypress */
+
 describe('Pokedex Pokemon', () => {
   it('make sure the list of pokemons is displayed', () => {
     // visit the app root
-    cy.visit('/');
+    cy.visit('/')
 
     // ensure the list is visible
-    cy.get('.list-item:nth-child(1)').should('be.visible');  
-     
-  }); 
+    cy.get('.list-item:nth-child(1)').should('be.visible')
+
+  })
 
   it('make sure the pokemon card is visible', () => {
     // visit the app root
-    cy.visit('/');
+    cy.visit('/')
 
     // ensure the list is visible
-    cy.get('.list-item:nth-child(1)').should('be.visible');
-    cy.get('.list-item:nth-child(1)').click(); 
-    cy.url().should('include', '/pokemon/bulbasaur'); 
-    cy.get('.pokemon-ability-name').should('contain.text', 'overgrow');
-  });
+    cy.get('.list-item:nth-child(1)').should('be.visible')
+    cy.get('.list-item:nth-child(1)').click()
+    cy.url().should('include', '/pokemon/bulbasaur')
+    cy.get('.pokemon-ability-name').should('contain.text', 'overgrow')
+  })
 
-});
+})

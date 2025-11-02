7ab191689c5534d3933859791b98a2b06298a4b3
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-11-02 14:24:34 -0600

    updating e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   e2e: {
     // baseUrl used by tests to visit the running app in CI
-    baseUrl: 'http://localhost:5000',
+    baseUrl: 'http://localhost:8081',
     // prefer tests in cypress/e2e; keep integration as a fallback
     specPattern: [
       'cypress/e2e/**/*.js',
---
+++ cypress/e2e/pokemon.cy.js
@@ -0,0 +1,22 @@
+describe('Pokedex Pokemon', () => {
+  it('make sure the list of pokemons is displayed', () => {
+    // visit the app root
+    cy.visit('/');
+
+    // ensure the list is visible
+    cy.get('.list-item:nth-child(1)').should('be.visible');  
+     
+  }); 
+
+  it('make sure the pokemon card is visible', () => {
+    // visit the app root
+    cy.visit('/');
+
+    // ensure the list is visible
+    cy.get('.list-item:nth-child(1)').should('be.visible');
+    cy.get('.list-item:nth-child(1)').click(); 
+    cy.url().should('include', '/pokemon/bulbasaur'); 
+    cy.get('.pokemon-ability-name').should('contain.text', 'overgrow');
+  });
+
+});

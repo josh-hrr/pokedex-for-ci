346255922a222a58c3aaafcf1101bc7b623caab1
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-11-02 13:33:32 -0600

    updated eslint issues

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,20 @@
 module.exports = {
   e2e: {
-  // baseUrl used by tests to visit the running app in CI
+    // baseUrl used by tests to visit the running app in CI
     baseUrl: 'http://localhost:5000',
-      // prefer tests in cypress/e2e; keep integration as a fallback
-      specPattern: [
-        'cypress/e2e/**/*.js',
-        'cypress/integration/**/*.js'
-      ],
-      setupNodeEvents(on, config) {
-        console.log('using on: ', on)
-        console.log('using config: ', config)
-        return config
-      },
-    },
-  video: false,
+    // prefer tests in cypress/e2e; keep integration as a fallback
+    specPattern: [
+      'cypress/e2e/**/*.js',
+      'cypress/integration/**/*.js'
+    ],
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      // eslint-disable-next-line no-console
+      console.log('using on: ', on)
+      // eslint-disable-next-line no-console
+      console.log('using config: ', config)
+      return config
+    }
+  },
+  video: false
 }

b13c485c8f81857a52f4f277354ee625d3365df4
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-11-02 13:31:10 -0600

    updated cypress config file'

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   e2e: {
   // baseUrl used by tests to visit the running app in CI
-      baseUrl: 'http://localhost:5000',
-        // prefer tests in cypress/e2e; keep integration as a fallback
-        specPattern: [
-          'cypress/e2e/**/*.js',
-          'cypress/integration/**/*.js'
-        ],
-        setupNodeEvents(on, config) {
-          console.log('using on: ', on)
-          console.log('using config: ', config)
-          return config
-        },
+    baseUrl: 'http://localhost:5000',
+      // prefer tests in cypress/e2e; keep integration as a fallback
+      specPattern: [
+        'cypress/e2e/**/*.js',
+        'cypress/integration/**/*.js'
+      ],
+      setupNodeEvents(on, config) {
+        console.log('using on: ', on)
+        console.log('using config: ', config)
+        return config
       },
+    },
   video: false,
 }

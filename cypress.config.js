d9f2393ff8a9fae7c2a37cc48b1d69e2ef25064e
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-11-02 11:52:56 -0600

    fixing eslint issues

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+      console.log("using on: ", on)
+      console.log("using config: ", config)
     },
   },
-};
+}
--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)

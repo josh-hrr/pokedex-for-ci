a55abe549ae96a5310aa8f04140875249ce87c96
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-11-02 13:22:35 -0600

    updated cypress config file'

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   e2e: {
+    // baseUrl used by tests to visit the running app in CI
+    baseUrl: 'http://localhost:5000',
+    // your existing tests are under cypress/integration/*.js
+    specPattern: 'cypress/integration/**/*.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       console.log('using on: ', on)
       console.log('using config: ', config)
+      return config
     },
   },
+  video: false,
 }

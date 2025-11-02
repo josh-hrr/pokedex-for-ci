96522c4a7f03de458248288fbe04146627ff40e0
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-11-02 11:55:51 -0600

    fixing eslit issues v2

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      console.log("using on: ", on)
-      console.log("using config: ", config)
+      console.log('using on: ', on)
+      console.log('using config: ', config)
     },
   },
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './styles.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles.css';
 
-
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+const root = createRoot(container);  
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
